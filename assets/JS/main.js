2c39029c3206df4c239dfbd4f4a5126e6d316011
James Wilcox <architeuthisdux@protonmail.com>
2018-10-31 20:52:54 -0700

    Initialized Firebase

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,3 +1,15 @@
 var googleKey = config.googleKey;
 var yelpKey = config.yelpKey;
 var yelpClient = config.yelpClientID;
+var dataKey = config.fireKey;
+
+// Initialize Firebase
+var config = {
+    apiKey: dataKey,
+    authDomain: "ucb-project-one.firebaseapp.com",
+    databaseURL: "https://ucb-project-one.firebaseio.com",
+    projectId: "ucb-project-one",
+    storageBucket: "ucb-project-one.appspot.com",
+    messagingSenderId: "191816644229"
+  };
+  firebase.initializeApp(config);
